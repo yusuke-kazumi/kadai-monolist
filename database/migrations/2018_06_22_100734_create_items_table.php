41ee7b38496ce3e37aed91778889e39b15b9dae7
yusuke.kazumi <yusuke.a.kazumi@rakuten.com>
2018-06-22 01:21:15 +0000

    rakuten.api

---
+++ database/migrations/2018_06_22_100734_create_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('name');
+            $table->string('url');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
