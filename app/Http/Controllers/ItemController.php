d6468cceba4c3aab3fd073543914b4e89f84ac84
yusuke.kazumi <yusuke.a.kazumi@rakuten.com>
2018-06-22 03:15:15 +0000

    want

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -33,4 +33,14 @@
             'items' => $items,
         ]);
     }
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
   }
